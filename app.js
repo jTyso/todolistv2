bce576ba9bdcbce54a416dd35a733d08465a116e
Jordan Theisen <jordantheisen08@gmail.com>
2020-04-03 10:13:05 -0500

    trying to fix error

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.static("public"));
-////mongodb+srv://admin-jordan:Baddog8118@cluster0-ckto6.mongodb.net/test?retryWrites=true&w=majority
+
 //mongodb://localhost:27017/todolistDB
 mongoose.connect("mongodb+srv://admin-jordan:test123@cluster0-ckto6.mongodb.net/test?retryWrites=true&w=majority", {
   useNewUrlParser: true,
